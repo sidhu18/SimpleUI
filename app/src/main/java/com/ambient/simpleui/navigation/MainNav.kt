75725aa11bee8c758986b415f79f3eb21c7322c3
sidhu18 <sidharth.v.mtr@gmail.com>
2021-01-01 20:12:23 +0530

    Add navigation to post screen from home

--- app/src/main/java/com/ambient/simpleui/navigation/MainNav.kt
+++ app/src/main/java/com/ambient/simpleui/navigation/MainNav.kt
@@ -1,12 +1,13 @@
 package com.ambient.simpleui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
 import com.ambient.simpleui.screens.home.ui.HomeScreen
-import com.ambient.simpleui.screens.post.ui.PostScreen
-import com.ambient.simpleui.screens.post.ui.components.Post
+import com.ambient.simpleui.screens.post.ui.*
 
 @Composable
 fun MainNav() {
@@ -20,8 +21,22 @@ fun MainNav() {
             HomeScreen(navController)
         }
 
-        composable(Screen.Post.route.name) {
-            PostScreen(navController)
+        composable(
+            route = "${Screen.Post.route.name}/{$ARG_POST_NAME}/{$ARG_POST_COUNTRY}/{$ARG_POST_IMAGE_RES}/{$ARG_POST_DESCRIPTION}",
+            arguments = listOf(
+                navArgument(ARG_POST_NAME) { type = NavType.StringType },
+                navArgument(ARG_POST_COUNTRY) { type = NavType.StringType },
+                navArgument(ARG_POST_IMAGE_RES) { type = NavType.IntType },
+                navArgument(ARG_POST_DESCRIPTION) { type = NavType.StringType },
+            )
+        ) {
+            PostScreen(
+                navController,
+                it.arguments?.getString(ARG_POST_NAME)!!,
+                it.arguments?.getString(ARG_POST_COUNTRY)!!,
+                it.arguments?.getInt(ARG_POST_IMAGE_RES)!!,
+                it.arguments?.getString(ARG_POST_DESCRIPTION)!!
+            )
         }
     }
 }
